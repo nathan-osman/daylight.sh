b867cb7854f9a236f19021be1977c180cd526e54
Nathan Osman <nathan@quickmediasolutions.com>
2019-06-10 22:41:35 -0700

    Fix output for debug statement.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from os import path
+from sys import stderr
 
 from geoip2.database import Reader
 from flask import Flask, jsonify, render_template, request
@@ -33,7 +34,7 @@ def _geolocate(ip):
     the response is an empty map
     """
     try:
-        print("geolocating {}...".format(ip))
+        print("geolocating {}...".format(ip), file=stderr)
         d = reader.city(ip)
         l = []
         if d.city.name is not None:
